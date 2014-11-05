33cc47946621d75ad273f847117513afba38b353
Steven Pigeon <steven_pigeon@uqar.ca>
2014-11-05 14:32:00 -0500

    Revert "Enlevé les ternaires (s=="") ? v1 : v2 et remplacé par"
    This reverts commit a3720d286da81efedfe3cfa39e2acdef3a61891a.

--- gps/gps-stream-decoder.cpp
+++ gps/gps-stream-decoder.cpp
@@ -8,23 +8,20 @@ gps::gps_data * gps_stream_decoder::decode(const std::string & gps_sentence)
 {
   std::string type=gps_sentence.substr(1,5);
 
-  try
-   {
-    if (type=="GPRMC")
-     return new gps::gps_data_position(gps_sentence);
+  if (type=="GPRMC")
+   return new gps::gps_data_position(gps_sentence);
+  else
+   if (type=="GPGGA")
+    return new gps::gps_data_fix(gps_sentence);
+   else
+    if (type=="GPGSA")
+     return new gps::gps_data_overall_satellite_info(gps_sentence);
     else
-     if (type=="GPGGA")
-      return new gps::gps_data_fix(gps_sentence);
+     if (type=="GPGSV")
+      return new gps::gps_data_satellites(gps_sentence);
      else
-      if (type=="GPGSA")
-       return new gps::gps_data_overall_satellite_info(gps_sentence);
-      else
-       if (type=="GPGSV")
-        return new gps::gps_data_satellites(gps_sentence);
-       else
-        return new gps::gps_data_unsupported(gps_sentence);
-   }
-  catch (...) { return new gps::gps_data_error(gps_sentence); }
+      return new gps::gps_data_unsupported(gps_sentence);
+
   return 0;
 }
 
